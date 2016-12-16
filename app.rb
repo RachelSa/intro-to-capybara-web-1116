09fd13a76b11a0516e7ddc870168ae69226b0283
github username <github email address>
2016-12-16 06:27:47 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 class Application < Sinatra::Base
   # write your code here!
+
+  get '/greet' do 
+    erb :greet
+  end
+
+  post '/greet' do
+    erb :greet
+  end
+
+  get '/' do 
+    erb :index
+  end
 end
 
